a3652f20966ae46996016f621b4652f337369439
Kieran Sonter <ksonter95@gmail.com>
2026-01-04 02:36:49 +1000

    modules: nixos: system: networking: removed DHCP option

--- modules/nixos/system/networking/default.nix
+++ modules/nixos/system/networking/default.nix
@@ -1,7 +1,7 @@
 #
 # Description:
 #   Default networking policy. This module enables NetworkManager and configures all
-#   network interfaces to use DHCP with a firewall.
+#   network interfaces with a firewall.
 #
 # Usage:
 #   Import this module from a machine configuration:
@@ -19,8 +19,6 @@
 
   # Enable NetworkManager as the primary network management service
   networking.networkmanager.enable = true;
-  # Enable DHCP for all network interfaces by default
-  networking.useDHCP = true;
   # Enable the system firewall with default rules
   networking.firewall.enable = true;
 }
