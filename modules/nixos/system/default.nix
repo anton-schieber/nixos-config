32e106566c0dbe87694c9feacc0aa71bc05aa359
Kieran Sonter <ksonter95@gmail.com>
2026-01-04 02:13:26 +1000

    modules: nixos: system: initial commit

---
+++ modules/nixos/system/default.nix
@@ -0,0 +1,16 @@
+#
+# Description:
+#   Default system policy.
+#
+# Usage:
+#   Import this module from a machine configuration:
+#       imports = [
+#         ../../modules/nixos/system
+#       ];
+#
+
+{ ... }:
+
+{
+  # No system features are configured by default
+}
